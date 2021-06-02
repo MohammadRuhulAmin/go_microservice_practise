2b8135e8d71f1a57073398eba0d5bc405a9149d6
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-06-02 18:03:49 +0600

    Practise-1

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main(){
 	http.HandleFunc("/",func(rw  http.ResponseWriter, r *http.Request ){
 		fmt.Println("hi there")
 	})
+
 	http.HandleFunc("/register",func(rw http.ResponseWriter, r *http.Request){
 		log.Println("register Section....!")
 		registeredList :=[]string{"sakib","Sajid","Ashraf"}
@@ -20,7 +21,18 @@ func main(){
 			log.Println(key, " ",val)
 		}
 	})
-
+	http.HandleFunc("/login",func(rw http.ResponseWriter, r*http.Request){
+		fmt.Println("Login Section...!")
+	})
+	http.HandleFunc("/contact",func(rw http.ResponseWriter, r *http.Request){
+		myInformation := make(map[string]string)
+		myInformation["name"]= "Md Ruhul Amin"
+		myInformation["college"]="NotreDame College"
+		myInformation["University"]="American International University-Bangladesh"
+		for key,val := range  myInformation{
+			fmt.Println(key,"  :=>   ", val)
+		}
+	})
 	http.ListenAndServe(":8000",nil)
 
 
